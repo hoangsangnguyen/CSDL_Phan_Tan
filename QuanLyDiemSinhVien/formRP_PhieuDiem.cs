fb4abfe3744c160306e7e489732ab57c5aabbe71
phananh9x <phananh123qqq@gmail.com>
2017-12-01 22:26:33 +0700

    update report phieu diem

--- QuanLyDiemSinhVien/formRP_PhieuDiem.cs
+++ QuanLyDiemSinhVien/formRP_PhieuDiem.cs
@@ -21,12 +21,16 @@ namespace QuanLyDiemSinhVien
         {
             String maSV = txtMaSV.Text.ToString();
             DataTable dt = new DataTable();
+            DataTable dt2 = new DataTable();
             String strLenh = "EXEC sp_InPhieuDiemCaNhan N'" + maSV + "'";
+            String strLenh2 = "EXEC sp_TimSinhVienTheoMa N'" + maSV + "'";
             //MessageBox.Show(strLenh);
             dt = Program.ExecSqlDataTable(strLenh);
+            dt2 = Program.ExecSqlDataTable(strLenh2);
             rp_InPhieuDiemCaNhan rp = new rp_InPhieuDiemCaNhan();
 
-            rp.SetDataSource(dt);
+            rp.Database.Tables[0].SetDataSource(dt);
+            rp.Database.Tables[1].SetDataSource(dt2);
             crystalReportViewer1.ReportSource = rp;
 
         }
